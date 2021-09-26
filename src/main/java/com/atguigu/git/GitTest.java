16daa2cbd297e74139f434b266148049546a7730
victor <victor2021@163.com>
2021-09-26 21:18:29 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git1!");
+        System.out.println("hello git 1!");
+        System.out.println("hello git 2!");
     }
 }
