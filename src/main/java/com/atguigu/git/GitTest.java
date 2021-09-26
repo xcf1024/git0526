b489e7e89427f7f9b58b2e0e3f7d1259a06db16e
victor <victor2021@163.com>
2021-09-26 21:16:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author xzq
+ * @Date 2021/9/25 16:18
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
